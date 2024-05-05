2e709be0dfb870c6c7d05eb826cfafd3adc24947
lixtelnis <lixtelnis@gmail.com>
2024-05-04 23:03:09 -0400

    rss

--- build.py
+++ build.py
@@ -9,6 +9,7 @@ def build_page(page_meta):
   page, meta = page_meta
   title = meta.get("title", "[[Add a title in meta.json]]")
   description = meta.get("description", "[[Add a description in meta.json]]")
+  date = meta.get("date", [0, 0, 0])
   ishome = meta.get("ishome", False)
   needsmath = meta.get("needsmath", False)
 
@@ -16,9 +17,16 @@ def build_page(page_meta):
   page_name = path.basename(page_dir)
   body = open(f"{page_dir}/body.html").read()
 
+  if ishome:
+    page_name = ""
+  if not path.exists(f"build/{page_name}"):
+    mkdir(f"build/{page_name}")
+
   page_out = main_layout
   page_out = page_out.replace("[[TITLE]]", title)
   page_out = page_out.replace("[[DESCRIPTION]]", description)
+  page_out = page_out.replace("[[DATE]]", f"{date[0]}-{date[1]}-{date[2]}")
+  page_out = page_out.replace("[[URL]]", f"https://blog.lixtelnis.com/{page_name}")
   page_out = page_out.replace("[[SITEINDEX]]", site_index)
   page_out = page_out.replace("[[BODY]]", body)
   if needsmath:
@@ -26,10 +34,6 @@ def build_page(page_meta):
   else:
     page_out = page_out.replace("[[MATH]]", "")
 
-  if ishome:
-    page_name = ""
-  if not path.exists(f"build/{page_name}"):
-    mkdir(f"build/{page_name}")
   file_out = open(f"build/{page_name}/index.html","w")
   file_out.write(page_out)
   file_out.close()
@@ -45,6 +49,31 @@ def build_index(page_metas):
   index += '\n</ul>'
   return index
 
+def build_rss(page_metas):
+  page_metas = filter(lambda page_meta: not page_meta[1].get("ishome", False), page_metas)
+
+  with open(f"build/rss.xml","w") as f:
+    f.write('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"><channel>')
+    f.write('<title>blog.lixtelnis.com</title>')
+    f.write('<link>https://blog.lixtelnis.com/</link>')
+
+    for page, meta in page_metas:
+      title = meta.get("title", "[[Add a title in meta.json]]")
+      description = meta.get("description", "[[Add a description in meta.json]]")
+      date = meta.get("date", [0, 0, 0])
+
+      page_dir = path.dirname(page)
+      page_name = path.basename(page_dir)
+
+      f.write('<item>')
+      f.write(f'<title>{title}</title>')
+      f.write(f'<description>{description}</description>')
+      f.write(f'<pubDate>{date[0]}-{date[1]}-{date[2]}</pubDate>')
+      f.write(f'<link>https://blog.lixtelnis.com/{page_name}</link>')
+      f.write('</item>')
+
+    f.write('</channel></rss>')
+
 pages = glob("src/pages/*/meta.json")
 page_metas = [(page, json.load(open(page))) for page in pages]
 
@@ -52,3 +81,5 @@ site_index = build_index(page_metas)
 
 for page_meta in page_metas:
   build_page(page_meta)
+
+build_rss(page_metas)
