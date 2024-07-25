8b8e713ba819d51d0c92add686986fdf319f3bbf
lixtelnis <lixtelnis@gmail.com>
2024-07-25 14:15:43 -0400

    less indexing complaints

--- build.py
+++ build.py
@@ -42,7 +42,7 @@ def build_page(page_meta):
 def build_index(page_metas):
   page_metas = filter(lambda page_meta: not page_meta[1].get("ishome", False), page_metas)
   page_metas = sorted(page_metas, key = lambda page_meta: page_meta[1].get("date", [0,0,0]), reverse=True)
-  index = '<ul>\n<h1>☰</h1>\n<div id="navhide"><li><a href="/">home</a></li>'
+  index = '<ul>\n<h2>☰</h2>\n<div id="navhide"><li><a href="/">home</a></li>'
   for page, meta in page_metas:
     page_dir = path.dirname(page)
     page_name = path.basename(page_dir)
